a673feea8d03e4a6835bdc2728b5e5188381b4e0
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-14 09:52:14 -0600

    home, character, components with routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Characters from './components/Characters'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/characters" component={Characters}/>
+  </Switch>
+)
 
-export default App;
+export default App
