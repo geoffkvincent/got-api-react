ff98c45250b683c96d4b3bf8e5a805eec6f87003
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-15 09:30:05 -0600

    removed store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import store from './store'
+// import store from './store'
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render (
-  <Provider store={store}>
+  <Provider >
     <BrowserRouter>
       <App /> 
     </BrowserRouter>
